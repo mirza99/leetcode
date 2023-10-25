4cc2322341034f031bfa0ff8a6b317de168d61ba
Mirza Selimovic <mirza.selimovic@meligys.com>
2023-10-25 15:52:13 +0200

    Two sum

--- src/main/java/com/meligys/leetcode/TwoSum.java
+++ src/main/java/com/meligys/leetcode/TwoSum.java
@@ -1,9 +1,38 @@
 package com.meligys.leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TwoSum {
 
-    public static int[] twoSum(int[] nums, int target) {
-        // TODO: implement
-        return new int[1];
+    public static int[] twoSumApproachOne(int[] nums, int target) {
+
+        for (int i = 0; i< nums.length; i++) {
+
+            for (int j = 1; j < nums.length; j++) {
+                if (target - nums[i] == nums[j]) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+
+        return new int[]{0, 1};
+    }
+
+
+    public static int[] twoSumApproachTwo(int[] nums, int target) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int sub = target - nums[i];
+            if (map.containsKey(sub)) {
+                return new int[]{ map.get(sub), i };
+            }
+            map.put(nums[i], i);
+
+        }
+
+        throw new IllegalArgumentException("No result found");
     }
 }
--- src/test/java/com/meligys/leetcode/TwoSumTest.java
+++ src/test/java/com/meligys/leetcode/TwoSumTest.java
@@ -11,13 +11,31 @@ public class TwoSumTest {
 
     @ParameterizedTest
     @CsvSource(value = {
-            "2,7,11,15; 9; 0,1"
+            "2,7,11,15; 9; 0,1",
+            "3,2,4; 6; 1,2",
+            "3,3; 6; 0,1",
+            "3,2,3; 6; 0,2"
     }, delimiterString = ";")
-    void twoSumTes(String input, int target, String output) {
+    void twoSumApproachOneTest(String input, int target, String output) {
         int[] inputArray = Arrays.stream(input.split("\\s*,\\s*")).mapToInt(Integer::parseInt).toArray();
         int[] expectedArray = Arrays.stream(output.split("\\s*,\\s*")).mapToInt(Integer::parseInt).toArray();
 
-        int[] result = TwoSum.twoSum(inputArray, target);
+        int[] result = TwoSum.twoSumApproachOne(inputArray, target);
+
+        assertArrayEquals(expectedArray, result);
+    }
+    @ParameterizedTest
+    @CsvSource(value = {
+            "2,7,11,15; 9; 0,1",
+            "3,2,4; 6; 1,2",
+            "3,3; 6; 0,1",
+            "3,2,3; 6; 0,2"
+    }, delimiterString = ";")
+    void twoSumApproachTwoTest(String input, int target, String output) {
+        int[] inputArray = Arrays.stream(input.split("\\s*,\\s*")).mapToInt(Integer::parseInt).toArray();
+        int[] expectedArray = Arrays.stream(output.split("\\s*,\\s*")).mapToInt(Integer::parseInt).toArray();
+
+        int[] result = TwoSum.twoSumApproachTwo(inputArray, target);
 
         assertArrayEquals(expectedArray, result);
     }
